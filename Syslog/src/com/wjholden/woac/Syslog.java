65593df66733ed7051a3480841d85c09b4e5c920
William John Holden <wjholden@gmail.com>
2017-07-05 14:12:53 -0400

    Add files via upload

---
+++ Syslog/src/com/wjholden/woac/Syslog.java
@@ -0,0 +1,24 @@
+package com.wjholden.woac;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+public class Syslog {
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        InetAddress group = InetAddress.getByName("239.5.1.4");
+        try (MulticastSocket socket = new MulticastSocket(514)) {
+            socket.joinGroup(group);
+            byte[] buf = new byte[1500];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            while (true) {
+                socket.receive(packet);
+                System.out.println(new String(buf, 0, packet.getLength()));
+            }
+        }
+    }
+    
+}
